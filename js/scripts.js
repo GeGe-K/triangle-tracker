6e3b889e8ba1c2e3a619d43c04bbccb268c22072
GeGe-K <gloriagivondo@gmail.com>
2018-09-17 07:17:04 +0300

    edited the control flows, condition

--- js/scripts.js
+++ js/scripts.js
@@ -8,17 +8,24 @@ var triangle = function(){
   var lengthB = sides[1]
   var lengthC = sides[2]
   // console.log(lengthA)
-  if (isNaN(lengthA) || isNaN(lengthB) || isNaN(lengthC)) {
-    alert("All sides MUST be numerical.");
-  } else if (lengthA >= (lengthB + lengthC) || lengthB >= (lengthC + lengthA) || lengthC >= (lengthA + lengthB)) {
-    alert("This is not a valid triangle!");
-  } else if (lengthA===lengthB && lengthB===lengthC) {
-    return "This is an Equilateral triangle";
+  // if (isNaN(lengthA) || isNaN(lengthB) || isNaN(lengthC)) {
+  //   alert("All sides MUST be numerical.");
+
+  if (lengthA===lengthB && lengthB===lengthC) {
+    // return "This is an Equilateral triangle";
+      document.getElementById('text_where').innerHTML = "This is an Equilateral triangle";
   } else if (lengthA===lengthB || lengthB===lengthC || lengthC===lengthB) {
-    return "This is an Isosceles trisngle.";
+    // return "This is an Isosceles trisngle.";
+      document.getElementById('text_where').innerHTML = "This is an Isosceles trisngle.";
   } else if (lengthA!=lengthB && lengthA!=lengthC) {
-    return "This is a Scalene triangle.";
+    // return "This is a Scalene triangle.";
+    document.getElementById('text_where').innerHTML = "This is a Scalene triangle.";
+  } else if (lengthA >= (lengthB + lengthC) || lengthB >= (lengthC + lengthA) || lengthC >= (lengthA + lengthB)) {
+   alert("This is not a valid triangle!");
   } else {
-    triangle("text_where").innerHTML = sides;
+    // return sides;
+    document.getElementById('text_where').innerHTML = sides
+
+
   }
 };
