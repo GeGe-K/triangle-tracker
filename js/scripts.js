c2dd81cf34ce58d31fb67dcb992db013725fbed6
GeGe-K <gloriagivondo@gmail.com>
2018-09-17 10:41:55 +0300

    fixed the indentation in the js script

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,11 @@ var triangle = function(){
   var lengthA = parseFloat(sides[0]);
   var lengthB = parseFloat(sides[1]);
   var lengthC = parseFloat(sides[2]);
-  console.log(typeof (lengthA))
-  console.log(lengthB)
-  console.log(lengthC)
   // if (isNaN(lengthA) || isNaN(lengthB) || isNaN(lengthC)) {
   //   alert("All sides MUST be numerical.");
-
   if (lengthA===lengthB && lengthB===lengthC) {
     // return "This is an Equilateral triangle";
-          document.getElementById('text_where').innerHTML = "This is an Equilateral triangle";
+    document.getElementById('text_where').innerHTML = "This is an Equilateral triangle";
   } else if (lengthA > (lengthB + lengthC) || lengthB > (lengthC + lengthA) || lengthC > (lengthA + lengthB)) {
    alert("This is not a valid triangle!");
  } else if (lengthA===lengthB || lengthB===lengthC || lengthC===lengthB) {
@@ -24,11 +20,8 @@ var triangle = function(){
   } else if (lengthA!=lengthB && lengthA!=lengthC && lengthB!=lengthC) {
     // return "This is a Scalene triangle.";
     document.getElementById('text_where').innerHTML = "This is a Scalene triangle.";
-
   } else {
     // return sides;
     document.getElementById('text_where').innerHTML = sides
-
-
   }
 };
